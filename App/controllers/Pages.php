8b73bd29102da607028f38ed217f8c7bdfd894bb
sumanofficial <officialofsuman@gmail.com>
2020-04-17 19:05:42 +0530

    Adding the Route Funtionity

---
+++ App/controllers/Pages.php
@@ -0,0 +1,14 @@
+<?php
+
+class Pages
+{
+    public function __construct()
+    {
+        echo "this is the page controller </br>";
+    }
+
+    public function index()
+    {
+        echo "This is the index function </br>";
+    }
+}
--- App/libraries/Core.php
+++ App/libraries/Core.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
     * App Core Class
     * Creates URL & Loads Core
@@ -8,16 +9,64 @@
 
 class Core
 {
-    protected $currentController = "pages";
-    protected $currentMethod = "index";
+    protected $currentController = 'pages';
+    protected $currentMethod = 'index';
     protected $currentParams = [];
 
     public function __construct()
     {
-        $this->getUrl();
+        $url = $this->getUrl();
+
+        // ===============================
+        // check if the first value exists
+
+        if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+            // set the value as the current controller
+            $this->currentController = ucwords($url[0]);
+
+            // unset the value
+            unset($url[0]);
+        }
+        // required the controller file
+        require_once '../app/controllers/' . $this->currentController . '.php';
+
+        // init the current controller
+        $this->currentController = new $this->currentController;
+
+        // =============================
+        //checks the second value exitts
+
+        if (isset($url[1])) {
+            // looks for the method in current controller
+            if (method_exists($this->currentController, $url[1])) {
+                // set the value
+                $this->currentMethod = $url[1];
+
+                // // unset the value 
+                unset($url[1]);
+            }
+        }
+        //echo $this->currentMethod . "</br>";
+
+        // ===============================
+        // Getting the Parameters
+        $this->currentParams = $url ? array_values($url) : [];
+
+        // call a callback with array of parameters
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->currentParams);
     }
     public function getUrl()
     {
-        echo $_GET["url"];
+        // check is url has set
+        if (isset($_GET['url'])) {
+            // trim the url
+            $url = rtrim($_GET['url'], '/');
+            // sanatize the url
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            // trun into array
+            $url = explode('/', $url);
+
+            return $url;
+        }
     }
 }
