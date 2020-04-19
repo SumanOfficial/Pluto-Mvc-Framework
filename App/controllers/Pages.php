85f2a1635351ab9988528086fb8b7eee02d8a169
sumanofficial <officialofsuman@gmail.com>
2020-04-20 00:28:21 +0530

    Add some default view Pages

--- App/controllers/Pages.php
+++ App/controllers/Pages.php
@@ -4,15 +4,10 @@ class Pages extends Controller
 {
     public function __construct()
     {
-        echo "this is the page controller </br>";
     }
 
     public function index()
     {
         echo "This is the index function </br>";
-    }
-    public function about()
-    {
-        $this->view('About');
     }
 }
